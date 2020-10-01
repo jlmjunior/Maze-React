29c532f8d62088f2bc52e2cf475c373444ba6dc6
Junior Martinez <jlmjunior@live.com>
2020-09-30 21:27:44 -0300

    Ajustes

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
+const conexao = async (startY, startX, endY, endX, url) => {
+  
+  let host = 'https://localhost:44342/'
+
+  let date = `api/Maze/${url}?startY=${startY}&startX=${startX}&endY=${endY}&endX=${endX}`
+
+  return await axios.get(host+date);
+};
+
 export const sendMaze = async (matriz, url) => {
   if (matriz === null || matriz === undefined) return null
 
@@ -27,10 +36,4 @@ export const sendMaze = async (matriz, url) => {
 
   return null    
 }
-
-const conexao = async (startY, startX, endY, endX, url) => {
-  let link = `https://localhost:44342/api/Maze/${url}?startY=${startY}&startX=${startX}&endY=${endY}&endX=${endX}`
-
-  return await axios.get(link);
-};
 
